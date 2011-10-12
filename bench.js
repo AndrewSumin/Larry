8be4b082296bd03afebde757350b20a675c3dff8
Andrew Sumin <AndrewSumin@gmail.com>
2011-10-12 19:28:36 +0400

    fix bench

--- bench.js
+++ bench.js
@@ -1,19 +1,17 @@
-var http = require('http');
-
+var i, start, http = require('http'), join = 0, parse = 0;
+for (i = 0; i < 100; i++){
 http.request({host:'dreadatour.ru', port:80, path:'/larry.js', method:'GET'}, function(response){
     var body = new Array();
     response.on('data', function(chunk){body.push(chunk)})
             .on('end', function(){
-                var i, join;
-                console.time('join');
-                for (i = 0; i < 100; i++){
-                    join = body.join('');
-                }
-                console.timeEnd('join');
-                console.time('parse');
-                for (i = 0; i < 100; i++){
-                    JSON.parse(join);
-                }
-                console.timeEnd('parse');
+                start = new Date().getTime();
+                body = body.join('');
+                join += (new Date().getTime() - start);
+                
+                start = new Date().getTime();
+                JSON.parse(body);
+                parse += (new Date().getTime() - start);
+                console.log(join, parse);
             });
 }).end()
+}
